6f788e056c63d911eb367b163b8df2a64d09f428
Fernando Rocha <fernandorsl@gmail.com>
2021-04-16 01:05:41 -0700

    Lab 2 - DOne

--- traveldeals/SessionManager.swift
+++ traveldeals/SessionManager.swift
@@ -6,6 +6,8 @@
 //
 
 import Amplify
+import AmplifyPlugins // Imports the Amplify plugin interface
+import AWSPinpoint    // Imports the AWSPinpoint client escape hatch
 
 //different auth states that a user can have
 enum AuthState {
@@ -33,6 +35,80 @@ final class SessionManager : ObservableObject {
     
     func showLogin() {
         authState = .login
+    }
+    
+    func identifyUser() {
+//        Amplify.Auth.fetchUserAttributes() { result in
+//                switch result {
+//                case .success(let attributes):
+//                    //print("User attributes - \(attributes)")
+//                    for attribute in attributes {
+//                        if attribute.key == AuthUserAttributeKey.email {
+//                            let email = attribute.value
+//
+//                            do {
+//                                //create escape hatch
+//                                let plugin = try Amplify.Analytics.getPlugin(for: "awsPinpointAnalyticsPlugin") as! AWSPinpointAnalyticsPlugin
+//                                let awsPinpoint = plugin.getEscapeHatch()
+//
+//                                //create batch item to update channel type
+//                                let batchItem = AWSPinpointTargetingEndpointBatchItem()
+//
+//                                batchItem?.channelType = AWSPinpointTargetingChannelType.email
+//                                batchItem?.address = email
+//
+//                                //create batch request w/ batch item(this is where I'm stuck)
+//                                let batchRequest = AWSPinpointTargetingUpdateEndpointsBatchRequest()
+//                                batchRequest.
+//
+//                                //update endpoint with the request
+//                                AWSPinpointTargeting.default().updateEndpointsBatch(...)
+//
+//                                AnalyticsUserProfile.
+//
+//                                Amplify.Analytics.iden
+//
+//                            } catch {
+//                                print("Get escape hatch failed with error - \(error)")
+//                            }
+//                            break
+//                        }
+//                    }
+//                case .failure(let error):
+//                    print("Fetching user attributes failed with error \(error)")
+//                }
+//            }
+
+//        Amplify.Auth.fetchUserAttributes() { result in
+//                   switch result {
+//                   case .success(let attributes):
+//                    for attribute in attributes {
+//                        if attribute.key == AuthUserAttributeKey.email{
+//                            let email = attribute.value
+//
+//                            let plugin = try Amplify.Analytics.getPlugin(for: "awsPinpointAnalyticsPlugin") as! AWSPinpointAnalyticsPlugin
+//                            let awsPinpoint = plugin.getEscapeHatch()
+//
+//                            let pinpointTargetingClient =
+//                                AWSMobileClient.sharedInstance.pinpoint!.targetingClient
+//                            pinpointTargetingClient.addAttribute(["Lakers","Clippers"],
+//                                forKey: "favoriteTeams")
+//                            pinpointTargetingClient.updateEndpointProfile()
+//
+//
+////                            guard let user = Amplify.Auth.getCurrentUser() else {
+////                                print("Could not get user, perhaps the user is not signed in")
+////                                return
+////                            }
+//
+//                            break
+//                        }
+//                    }
+//
+//                   case .failure(let error):
+//                       print("Fetching user attributes failed with error \(error)")
+//                   }
+//               }
     }
     
     func signUp(username: String, email: String, password: String){
@@ -97,12 +173,13 @@ final class SessionManager : ObservableObject {
             switch result {
             case .success(let signInResult):
                 print(signInResult)
+                
                 if signInResult.isSignedIn {
                     DispatchQueue.main.async {
                         self?.getCurrentAuthUser()
+                        self?.identifyUser()
                     }
                 }
-                
             case .failure(let error):
                 print("Login error: ", error)
                 self?.signOut()
@@ -120,6 +197,10 @@ final class SessionManager : ObservableObject {
                     self?.getCurrentAuthUser()
                 }
                 
+                //send logout event to pinpoint
+                let event = BasicAnalyticsEvent(name: "user-signOut")
+                Amplify.Analytics.record(event: event)
+                
                 //Clear local data
                 Amplify.DataStore.clear() { result in
                         switch result {
@@ -135,4 +216,5 @@ final class SessionManager : ObservableObject {
             }
         }
     }
+    
 }
--- traveldeals/traveldealsApp.swift
+++ traveldeals/traveldealsApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 import Amplify
 import AmplifyPlugins
+import AWSPinpoint
+import UserNotifications
+
 
 @main
 struct traveldealsApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
     @ObservedObject var sessionManager = SessionManager()
     
     var body: some Scene {
@@ -37,6 +42,9 @@ struct traveldealsApp: App {
     }
 
     init() {
+        
+        registerForPushNotifications()
+        
         do{
             //Auth
             try Amplify.add(plugin:AWSCognitoAuthPlugin())
@@ -60,4 +68,82 @@ struct traveldealsApp: App {
         //gets current authenticated user
         sessionManager.getCurrentAuthUser()
     }
+    
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options:[.alert, .sound, .badge]) { granted, error in
+                print("Permission granted: \(granted)")
+                guard granted else { return }
+
+                // Only get the notification settings if user has granted permissions
+                self.getNotificationSettings()
+        }
+    }
+
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+
+            DispatchQueue.main.async {
+                // Register with Apple Push Notification service
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+
+}
+
+//*** Implement App delegate ***//
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    var pinpoint: AWSPinpoint?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        do {
+            let plugin = try Amplify.Analytics.getPlugin(for: "awsPinpointAnalyticsPlugin") as! AWSPinpointAnalyticsPlugin
+            pinpoint = plugin.getEscapeHatch()
+            print("Sucessfully got AWSPinpoint instance from escape hatch")
+        } catch {
+            print("Get escape hatch failed with error - \(error)")
+        }
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+
+        // Register the device token with Pinpoint as the endpoint for this user
+        pinpoint!.notificationManager.interceptDidRegisterForRemoteNotifications(withDeviceToken: deviceToken)
+    }
+
+    func application(_ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+
+    func application(_ application: UIApplication,
+                      didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                      fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+
+        if (application.applicationState == .active) {
+            let alert = UIAlertController(title: "Notification Received",
+                                          message: userInfo.description,
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+
+            let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+            
+            keyWindow?.rootViewController?.present(
+                alert, animated: true, completion:nil)
+        }
+
+        pinpoint!.notificationManager.interceptDidReceiveRemoteNotification(userInfo)
+    }
+    
 }
